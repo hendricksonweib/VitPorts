c1d2fef9d4b7d46bddf0279286f74a7bc592b6e6
hendricksonweib <hendricksonrocha22@gmail.com>
2025-07-14 23:02:36 -0300

    feat: add imagens

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -2,18 +2,18 @@ import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import LogoGrupoMateus from "../../../public/logogrupomateus.png"
+import Logo from "../../../public/logo.png"
 
 export function LoginForm({
   className,
   ...props
 }: React.ComponentProps<"form">) {
   return (
-    <form className={cn("flex flex-col gap-6", className)} {...props}>
-      <div className="flex flex-col items-center gap-2 text-center">
-        <img src={LogoGrupoMateus} alt="" />
+    <form className={cn("flex flex-col", className)} {...props}>
+      <div className="flex flex-col items-center text-center">
+        <img src={Logo} alt="" />
       </div>
-      <div className="grid gap-6">
+      <div className="grid gap-4">
         <div className="grid gap-3">
           <Label htmlFor="email">Usuário</Label>
           <Input id="email" type="email" placeholder="Digite seu usuário" required />
