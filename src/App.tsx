0fdcb50257c5075e58577d56ea49f3e0cd1df2b2
hendricksonweib <hendricksonrocha22@gmail.com>
2025-07-14 22:38:43 -0300

    feat:routes dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,28 @@
-import { AppSidebar } from "@/components/app-sidebar"
-import { SiteHeader } from "@/components/site-header"
-import { ThemeProvider } from "@/features/theme-provider"
+import { Routes, Route, Navigate } from "react-router-dom"
+import LoginPage from "./app/login/LoginPage"
+import DashboardLayout from "./layout/DashboardLayout"
+import HomePage from "./app/dashboard/HomePage"
 
-import {
-  SidebarInset,
-  SidebarProvider,
-} from "@/components/ui/sidebar"
-
-export const iframeHeight = "800px"
-
-export const description = "A sidebar with a header and a search form."
+const isAuthenticated = false
 
 export default function App() {
   return (
-   <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-    <div className="[--header-height:calc(--spacing(14))]">
-      <SidebarProvider className="flex flex-col">
-        <SiteHeader />
-        <div className="flex flex-1">
-          <AppSidebar />
-          <SidebarInset>
-            <div className="flex flex-col items-center justify-center h-full">
-              <h1 className="text-2xl font-bold">Welcome to the Dashboard</h1>
-              <p className="mt-4 text-gray-600">This is a simple sidebar layout with a header and a search form.</p>
-            </div>
-          </SidebarInset>
-        </div>
-      </SidebarProvider>
-    </div>
-  </ThemeProvider>  
+    <Routes>
+      <Route
+        path="/"
+        element={
+          isAuthenticated ? <Navigate to="/dashboard" /> : <LoginPage />
+        }
+      />
+
+      <Route
+        path="/dashboard"
+        element={
+          isAuthenticated ? <DashboardLayout /> : <Navigate to="/" />
+        }
+      >
+        <Route index element={<HomePage />} />
+      </Route>
+    </Routes>
   )
 }
